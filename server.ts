8f8b25113bd8e8bd85b14cc098e1523c4495db93
Jason Guo <jasonyzguo@gmail.com>
2021-05-06 22:10:53 -0400

    fix app port

--- server.ts
+++ server.ts
@@ -26,6 +26,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
   })
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
     console.log(`Listening on port ${port}!`)
 })
